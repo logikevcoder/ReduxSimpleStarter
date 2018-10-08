3cce80c031075764594af6bc326fb0e6ec778b56
“Kevin <logikevcoder@gmail.com>
2018-10-07 21:40:46 -0500

    added component base files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const api_key = 'AIzaSyAuOMlo2rK_H824NWJL0KX_YylL8CE0BIk';
+// Create a new component
+const App = () => {
+    return  (
+      <div>
+        <SearchBar />
+      </div>
+    );
+}
+// Should produce some html
+// Take the components (generated html) and display in the dom ( on the page )
+ReactDOM.render(<App />, document.querySelector('.container'));
