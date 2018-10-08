90451032241a056562b4da2b4b3d21271ddc3ee7
“Kevin <logikevcoder@gmail.com>
2018-10-07 22:26:43 -0500

    converted app functional component to be class based, added props for the youtube search function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCyWzW8MsUhFD6aRAcM2nDC2ZDW9pND6Jg';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }) // this.setState({ videos: videos })
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  } 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
