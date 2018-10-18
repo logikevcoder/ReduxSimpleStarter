907c76f6c0bfa7c41b512b578eb717636645b543
“Kevin <logikevcoder@gmail.com>
2018-10-18 10:41:41 -0500

    created the video list component and state

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCyWzW8MsUhFD6aRAcM2nDC2ZDW9pND6Jg';
 
 
 
 class App extends Component {
+
+  // always gets called with props 
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = { videos: []}; // contains a list of objects
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos }) // this.setState({ videos: videos })
     });
   }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   } 
